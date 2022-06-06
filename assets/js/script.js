96cbf8ca59f8bdf0af0af0df901eb9c77fac458d
Trevor Bos <trevor.bos@outlook.com>
2022-06-06 18:27:56 -0400

    added some pseudo coding to the JS file, looking into some things that might be needed.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var pageStart = function () {
+
+    // Variables and potential arrays that will be needed
+
+    // Get the API key and assign it a variable.
+
+    // Get the current weather from open weather api
+
+    // Get the 5 day forecast for the specific city entered
+
+    // Find the history from local storage
+    
+    // Render out the search history
+    
+    // Clear history button functionality
+
+
+}
+
+pageStart ();
