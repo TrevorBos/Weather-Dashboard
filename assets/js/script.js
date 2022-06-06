2d78a34aeb6b9022142b8be966e223b36b6917cc
Trevor Bos <trevor.bos@outlook.com>
2022-06-06 19:29:29 -0400

    started the weather function trying to get the information from openWeather.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,14 +25,24 @@ var pageStart = function () {
     var openWeatherAPIKey = "ab616e87112839dadb80a8304e9cff29";
 
     // Get the current weather from open weather api
-    var findWeatherForChosenCity (cityName) {
+    var findWeatherForChosenCity = function (cityName) {
         var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openWeatherAPIKey;
         fetch(cityUrl).then(function(response){
             currentWeather.classList.remove("d-none");
 
             //Display the current weather
             var currentDate = new Date(response.formData.dt * 1000);
-            
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth() + 1;
+            var year = currentDate.getFullYear();
+
+            chosenCityName.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            var weatherImage = response.data.weather[0].icon;
+            currentWeatherImg.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherImage + "@2x.png");
+            currentWeatherImg.setAttribute("alt", response.data.weather[0].description);
+            currentTemp.innerHTML = "Temperature: " + response.data.main.temp + " &#176f";
+            currentHumidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindSpeed.innerHTML = "Wind Speed: " + response.data.wind.speed + "MPH";
         })
     }
 
